7859cb86dd29ed096045d297ec3d902d1d8c3ecb
Jeon <hamham300@naver.com>
2020-04-29 03:02:23 +0900

    2020-04-29 03:02 리뷰작성 다이얼로그 레이아웃

--- app/src/main/java/kr/co/hwakjjin/ssmeomuk/ReviewData.kt
+++ app/src/main/java/kr/co/hwakjjin/ssmeomuk/ReviewData.kt
@@ -10,5 +10,44 @@ class ReviewData(
     Up: Int,
     Date: Int
 ) {
+    private var code = ""
+    private var ID = ""
+    private var Review = ""
+    private var Rate = 0f
+    private var Up = 0
+    private var Date = 0
+
+    init {
+        this.code = code
+        this.ID = ID
+        this.Review = Review
+        this.Rate = Rate
+        this.Up = Up
+        this.Date = Date
+    }
+
+    fun getCode(): String?{
+        return code
+    }
+
+    fun getID(): String?{
+        return ID
+    }
+    fun getReview(): String?{
+        return Review
+    }
+
+    fun getRate(): Float?{
+        return Rate
+    }
+
+    fun getUp(): Int?{
+        return Up
+    }
+
+    fun getDate(): Int?{
+        return Date
+    }
+
 
 }
--- app/src/main/java/kr/co/hwakjjin/ssmeomuk/ReviewListAdapter.kt
+++ app/src/main/java/kr/co/hwakjjin/ssmeomuk/ReviewListAdapter.kt
@@ -1,4 +1,45 @@
 package kr.co.hwakjjin.ssmeomuk
 
-class ReviewListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class ReviewListAdapter (val context: Context, val data: ArrayList<ReviewData>, val itemClick: (ReviewData)-> Unit) : RecyclerView.Adapter<ReviewListAdapter.Holder>() {
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    inner class Holder(view: View?, itemClick: (ReviewData) -> Unit) : RecyclerView.ViewHolder(view!!){
+        val reviewer  = view?.findViewById<TextView>(R.id.reviewer)
+        val reviewTxt =  view?.findViewById<TextView>(R.id.txt_review)
+        val reviewUp = view?.findViewById<TextView>(R.id.reviewUp)
+        val rate = view?.findViewById<RatingBar>(R.id.review_rate)
+
+        fun bind(review: ReviewData, context: Context , itemCount: Int){
+
+            reviewer?.text = review.getID()
+            reviewTxt?.text = review.getReview()
+            reviewUp?.text = review.getUp().toString()
+            rate?.rating = review.getRate()!!.toFloat()
+
+            itemView.setOnClickListener { itemClick(review) }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.listitem_review, parent, false)
+        return Holder(view, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(data[position], context, position)
+    }
 }
