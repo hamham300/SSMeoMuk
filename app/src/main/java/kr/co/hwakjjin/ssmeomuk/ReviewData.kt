4721914ee88970576db44837314775d71a8c764f
Jeon <hamham300@naver.com>
2020-04-29 01:56:31 +0900

    2020-04-29 01:56 별점오류해결, 리뷰리사이클러뷰 준비

---
+++ app/src/main/java/kr/co/hwakjjin/ssmeomuk/ReviewData.kt
@@ -0,0 +1,14 @@
+package kr.co.hwakjjin.ssmeomuk
+
+import java.util.*
+
+class ReviewData(
+    code : String,
+    ID : String,
+    Review: String,
+    Rate: Float,
+    Up: Int,
+    Date: Int
+) {
+
+}
